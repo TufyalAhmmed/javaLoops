4dcc924b42c2ac034a951a775f703734ca947b4a
Tufyal Ahmmed <tufyal15-133@diu.edu.bd>
2023-05-28 02:02:47 +0600

    for & while loop

---
+++ src/forLoop/ForLoop.java
@@ -0,0 +1,14 @@
+package forLoop;
+
+public class ForLoop {
+    public static void main(String args[]){
+        for(int i=1;i<=10;i=i+2){
+            System.out.print(i);
+        }
+        for(int i=0;i<=10;i=i+2){
+            System.out.println(i);
+        }
+    }
+
+
+}
---
+++ src/whileLoop/WhileLoops.java
@@ -0,0 +1,11 @@
+package whileLoop;
+
+public class WhileLoops {
+    public static void main(String args[]){
+        int i = 0;
+        while (i<=10){
+            System.out.println(i);
+            i=i+2;
+        }
+    }
+}
